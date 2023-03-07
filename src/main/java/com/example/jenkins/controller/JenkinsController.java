d11eae042f140904c7f8b8f0372a5537023d1d50
xingyehuang <974966042@qq.com>
2023-03-07 15:51:25 +0800

    Jenkins

---
+++ src/main/java/com/example/jenkins/controller/JenkinsController.java
@@ -0,0 +1,15 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsController {
+
+    @GetMapping("/send")
+    public String send(String param) {
+        return param;
+    }
+}
